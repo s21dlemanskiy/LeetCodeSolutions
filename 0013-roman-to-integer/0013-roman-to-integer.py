8dcc55fb7bc5798b9a113e3ca63419d145189992
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2023-01-03 19:15:33 +0300

    Time: 124 ms (10.13%), Space: 13.8 MB (98.85%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,12 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        d = {0: 1, 1: 5, 2:10, 3: 50, 4: 100, 5: 500, 6: 1000 }
-        a = ["I", "V", "X", "L", "C", "D", "M"]
-        data = list(map(lambda x: a.index(x), list(s)))
+        d = {"I": 1, "V": 5, "X":10, "L": 50,"C": 100, "D": 500, "M": 1000 }
+        a = "IVXLCDM"
         result = 0
-        for i in range(len(data)):
-            if any(map(lambda x: x > data[i], data[i:])):
-                result -= d[data[i]]
+        for i in range(len(s)):
+            sign = s[i]
+            if i < len(s) - 1 and a.index(sign) < a.index(s[i + 1]):
+                result -= d[sign]
             else:
-                result += d[data[i]]
+                result += d[sign]
         return result
