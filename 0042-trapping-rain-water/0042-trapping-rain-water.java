d658c362e566438ecaccc36cf5ec1968a4b9ce5b
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2023-01-08 00:40:30 +0300

    Time: 143 ms (5.05%), Space: 43.4 MB (63.57%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,30 +1,29 @@
 public class Solution {
     public static int trap(int[] height) {
         int sum = 0;
-        for (int i = 0; i < height.length - 1; i++) {
-            int index_max = -1;
-            int sumtmp = 0;
-            int count = 0;
+        for (int i = 0; i < height.length - 1; i++) {  //левая граница нашей water trap
+            int index_max = -1;                         //правая граница нашей water trap: это индекс которое либо первое после левой границы с большей высотой чем height[i] или последнее наибольшее после левой границы ( если после левой границы все меньше или равны ей)
+            int sumtmp = 0;                             // временная сумма всоты воды если бы левая граница была выше правой. хранится пока не найдем новую правую границу.
+            int count = 0;                              // кол-во ячеек между левой и правой границей
             int counttmp = 0;
-            for (int j = i + 1; j < height.length; j++) {
-                if ( index_max == -1 || height[index_max] <= height[j]){
+            for (int j = i + 1; j < height.length; j++) { // проверяемая на место правой границы
+                if ( index_max == -1 || height[index_max] <= height[j]){   // если правая граница не установлена или установленая правая граница меньше чем эта то делаем текущее значение правой границей
                     index_max = j;
                     sum += sumtmp;
                     count += counttmp;
                     sumtmp = 0;
                     counttmp = 0;
-                    if (height[index_max] > height[i]){
+                    if (height[index_max] > height[i]){                 // если правая граница выше левой заканчиваем (тк для последующих значений она должна быть левой границей)
                         break;
                     }
                 }
                 sumtmp += height[i] - height[j];
                 counttmp++;
             }
-            if (height[i] > height[index_max]){
+            if (height[i] > height[index_max]){                     // если правая граница ниже опускаем уровень воды до нее(ранее мы считали воду по левой границы)
                 sum -= count * (height[i] - height[index_max]);
             }
-//            System.out.println(Integer.toString(i) + "-" + Integer.toString(index_max) + "-" + Integer.toString(sum));
-            i = index_max - 1;
+            i = index_max - 1;   // на следующем шаге i будет равно index_max те правая граница станет левой
         }
         return sum;
     }
