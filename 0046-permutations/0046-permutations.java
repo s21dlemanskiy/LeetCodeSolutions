276130f88073221a019db05eef7112e49011d115
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2023-01-09 07:08:00 +0300

    Time: 4 ms (21.00%), Space: 42.7 MB (66.54%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,6 +2,7 @@ class Solution {
     static int[] used; // масив содержащий последовательность выбранных эл-ов и их позиций например если used[2] = 3 это занчит что в результат поподет масив который на позиции 3 будет иметь nums[2]
     static List<List<Integer>> result; // список масивов, результат функции permute
     static int[] nums_pub;  // тот же масив nums просто обьявленый в глобальном пространстве
+    static int recursion_level; // уровень рекурсии
     public static void confirmResult(){  // добовление последовательности хранящийся в used в result
         int[] tmp_result = new int[used.length];
         for (int i = 0; i < used.length; i++) {
@@ -9,25 +10,28 @@ class Solution {
         }
         result.add(Arrays.stream(tmp_result).boxed().toList());
     }
-    public static void recursiveProduct(int n){  // рекурсивная функция нахождения декартого произведения
-        if (n == nums_pub.length){   // если used уже заполнен то нужно его записать в результат
+    public static void recursiveProduct(){  // рекурсивная функция нахождения декартого произведения
+        if (recursion_level == nums_pub.length){   // если used уже заполнен то нужно его записать в результат
             confirmResult();
             return;
         }
         for (int i = 0; i < used.length; i++) {  // если в used еще есть -1 нужно их рекурсивно заполнять
             if (used[i] == -1){
-                used[i] = n;    // меняем i-ую -1 на n
-                recursiveProduct(n + 1); // запускаем рекурсию дальше
+                used[i] = recursion_level;    // меняем i-ую -1 на уровень рекусии (от 0 до len(nums) -1 )
+                recursion_level++;
+                recursiveProduct(); // запускаем рекурсию дальше
+                recursion_level--;
                 used[i] = -1;          // чистим за собой возвращая i-ую -1
             }
         }
     }
     public static List<List<Integer>> permute(int[] nums) {
+        recursion_level = 0;
         nums_pub = nums;
         used = new int[nums.length];
         Arrays.fill(used, -1);
         result = new ArrayList<>();
-        recursiveProduct(0);
+        recursiveProduct();
         return result;
     }
 }
