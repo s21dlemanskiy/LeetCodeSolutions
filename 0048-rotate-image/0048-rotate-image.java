73a0f23246a837c355c3af255b08a3d6d83e011a
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2023-01-11 10:20:47 +0300

    Time: 0 ms (100.00%), Space: 41.4 MB (62.48%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,16 +1,14 @@
  class Solution {
-        public static void circle_swap(int[][] matrix, int x, int y){
-            // System.out.println(matrix[y][x]);
-             int tm = matrix[y][x];
+        public static void circle_swap(int[][] matrix, int x, int y){// в функцию передается координата точки лежащий в левой верхней четверти и она производит циклическое ее смещение
+            int tm = matrix[y][x];
             matrix[y][x] = matrix[matrix.length - 1 - x][y];
             matrix[matrix.length - 1 - x][y] = matrix[matrix.length - 1 - y][matrix.length - 1 - x];
             matrix[matrix.length - 1 - y][matrix.length - 1 - x] = matrix[x][matrix.length - 1 - y];
             matrix[x][matrix.length - 1 - y] = tm;
         }
         public static void rotate(int[][] matrix) {
-            for (int y = 0; y < matrix.length / 2; y++) {
-                for (int x = 0; x < matrix.length / 2 +  matrix.length % 2; x++) {
-                    // System.out.println(String.format("x:%d  y:%d val:%d \n", x, y, matrix[y][x]));
+            for (int y = 0; y < matrix.length / 2; y++) {                             // перебераем все х и у в первой получетверти
+                for (int x = 0; x < matrix.length / 2 +  matrix.length % 2; x++) {    // спорные х и у которые относятся к левой верхней и левой нижней не берем, а те которые к правой верхней и левой верхней берем
                     circle_swap(matrix, x, y);
                 }
             }
