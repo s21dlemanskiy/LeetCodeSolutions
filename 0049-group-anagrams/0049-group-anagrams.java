987e00c75258d0d19d9d516b4190376d704d4bab
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2023-01-11 11:16:57 +0300

    Time: 498 ms (5.01%), Space: 45.6 MB (78.41%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,4 +1,4 @@
-class Solution {
+ class Solution {
         public List<List<String>> groupAnagrams(String[] strs) {
             ArrayList<List<String>> result = new ArrayList<>();
             boolean[] in_list = new boolean[strs.length];
@@ -6,18 +6,18 @@ class Solution {
             for (int i = 0; i < strs.length; i++) {
                 char[] str = strs[i].toCharArray();
                 Arrays.sort(str);
-                sorted_strings[i] = new String(str);
+                sorted_strings[i] = new String(str);      // сортируем каждую строку в лексиграфичичском порядке
             }
-            for (int i = 0; i < strs.length; i++) {
-                if (in_list[i]){
+            for (int i = 0; i < strs.length; i++) {    // по каждой строке добавляя ее и ее анограммы в result
+                if (in_list[i]){                       // если мы уже добавили этот элимент в result то пропускаем его
                     continue;
                 }
                 ArrayList<String> groop = new ArrayList<>();
                 groop.add(strs[i]);
                 in_list[i] = true;
                 for (int j = i + 1; j < strs.length; j++) {
-                    if (sorted_strings[i].equals(sorted_strings[j])){
-                        groop.add(strs[j]);
+                    if (!in_list[j] && sorted_strings[i].equals(sorted_strings[j])){     // для каждой строки после данной проверяем является ли она ее анограмой
+                        groop.add(strs[j]);                                              // если да то добавляем ее в эту же группу
                         in_list[j] = true;
                     }
                 }
